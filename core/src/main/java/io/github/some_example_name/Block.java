b0d619b15684cb4ab03463043989b70a12923415
Ruhani05 <ruhani23450@iiitd.ac.in>
2024-10-27 23:31:00 +0530

    structure hardcoded

--- core/src/main/java/io/github/some_example_name/Block.java
+++ core/src/main/java/io/github/some_example_name/Block.java
@@ -6,10 +6,17 @@ import com.badlogic.gdx.scenes.scene2d.ui.Image;
 
 public class Block {
     private Image image;
+    private float xPercent, yPercent; // Store percentage positions
+    private float widthPercent, heightPercent; // Store percentage sizes
+
     float SCREEN_WIDTH = Gdx.graphics.getWidth();
     float SCREEN_HEIGHT = Gdx.graphics.getHeight();
     public Block(String texturePath, float x, float y, float width, float height) {
         Texture texture = new Texture(texturePath);
+        this.xPercent=x;
+        this.yPercent=y;
+        this.widthPercent=width;
+        this.heightPercent=height;
         this.image = new Image(texture);
         this.image.setPosition(SCREEN_WIDTH*x-width*0.5f,SCREEN_HEIGHT* y-height*0.5f);
         this.image.setSize(width, height);  // Resize to specified dimensions
@@ -18,4 +25,14 @@ public class Block {
     public Image getImage() {
         return image;
     }
+//    public void resize(float newWidth, float newHeight) {
+//        // Resize and reposition the block based on the new screen dimensions
+//        float newBlockWidth = newWidth * widthPercent;
+//        float newBlockHeight = newHeight * heightPercent;
+//        float newX = newWidth * xPercent - newBlockWidth * 0.5f;
+//        float newY = newHeight * yPercent - newBlockHeight * 0.5f;
+//
+//        this.image.setSize(newBlockWidth, newBlockHeight);
+//        this.image.setPosition(newX, newY);
+//    }
 }
